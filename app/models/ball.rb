bf35c10ceecd027ee6a24297e3b4394fe6c8e958
Luvic van Gool <vic@thecloudblocks.com>
2013-08-05 12:09:09 +0100

    show

--- app/models/ball.rb
+++ app/models/ball.rb
@@ -1,17 +1,17 @@
 class Ball < ActiveRecord::Base
 
-	attr_accessor :is_cached, :the_randomsize
-
 	def randomsize
 
-		if @the_randomsize.nil?
-			@is_cached = false
-			@the_randomsize = (Ball.count + 1) * Random.rand(99)
-		else
-			@is_cached = true
-		end
+		@the_randomsize ||= Random.rand(99)
 
-		@the_randomsize
+		# if @the_randomsize.nil?
+		# 	@is_cached = false
+		# 	@the_randomsize = (Ball.count + 1) * Random.rand(99)
+		# else
+		# 	@is_cached = true
+		# end
+
+		# @the_randomsize
 	end
 
 end
