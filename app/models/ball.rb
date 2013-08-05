05f06c295e532dab31b5b78e35a53ee2c9ee13f7
Luvic van Gool <vic@thecloudblocks.com>
2013-08-05 11:38:29 +0100

    show

--- app/models/ball.rb
+++ app/models/ball.rb
@@ -5,10 +5,10 @@ class Ball < ActiveRecord::Base
 	def randomsize
 
 		if @randomsize.nil?
-			@is_cached = true
+			@is_cached = false
 			@randomsize = (Ball.count + 1) * Random.rand(99)
 		else
-			@is_cached = false
+			@is_cached = true
 		end
 
 		@randomsize
