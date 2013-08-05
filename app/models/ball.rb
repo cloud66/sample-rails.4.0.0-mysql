a206822b4e2a07ac7e32b3d6869d2a2f815a95d4
Luvic van Gool <vic@thecloudblocks.com>
2013-08-05 11:21:56 +0100

    show

--- app/models/ball.rb
+++ app/models/ball.rb
@@ -1,63 +1,16 @@
 class Ball < ActiveRecord::Base
 
+	attr_accessor :is_cached
+
 	def randomsize
 
 		if @randomsize.nil?
-
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts "NOT CACHED --> FETCHING"
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
-			puts
+			@is_cached = true
 			@randomsize = (Ball.count + 1) * Random.rand(99)
+		else
+			@is_cached = false
 		end
+
 		@randomsize
 	end
 
