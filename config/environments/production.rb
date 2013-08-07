23b3417168920521a0e7973ab729d98a0389aee9
Luvic van Gool <vic@thecloudblocks.com>
2013-08-07 17:38:29 +0100

    env var

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,8 @@ Sample::Application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  
+  config.action_controller.perform_caching = false
 
   config.action_controller.page_cache_directory = "#{Rails.root.to_s}/public"
 
