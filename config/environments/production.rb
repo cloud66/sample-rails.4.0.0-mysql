90c4bf3ed8eec92ad0819fa6445ba79946537a0d
lvangool <luvic.vangool@gmail.com>
2013-08-23 12:09:23 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,9 +31,6 @@ Sample::Application.configure do
   config.assets.js_compressor  = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
   # Generate digests for assets URLs.
   config.assets.digest = true
 
